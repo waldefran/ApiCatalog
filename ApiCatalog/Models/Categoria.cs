55a19ff65d0b954081b1e560cdd214b9e1ca7eb9
waldefran <valdefranpereira@gmail.com>
2024-07-28 18:16:10 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCatalog/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiCatalog.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string ImagemUrl { get; set; }
+    public string Descricao { get; set; }
+}
